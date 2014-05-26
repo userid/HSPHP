3b6da03c246a2833402b184979bfd6a11c6e8d80
Gao Jie <jgao@changba.com>
2014-05-26 11:04:09 +0800

    only using for select sql; add filter_fields.

---
+++ src/HSPHP/hsphp.inc.php
@@ -0,0 +1,11 @@
+<?php
+
+require  __DIR__ . "/ReadCommandsInterface.php";
+#require  __DIR__ . "/WriteCommandsInterface.php";
+require  __DIR__ . "/Pipeline.php";
+require  __DIR__ . "/ErrorMessage.php";
+require  __DIR__ . "/IOException.php";
+#require  __DIR__ . "/ReadHandler.php";
+require  __DIR__ . "/ReadSocket.php";
+#require  __DIR__ . "/WriteHandler.php";
+#require  __DIR__ . "/WriteSocket.php";
